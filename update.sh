f3cc4b6d6f5f2e83eb5c1ebc3a563e4b58b0fe52
BinZFAKE9999 <99895433+BinZFAKE9999@users.noreply.github.com>
2024-02-13 16:12:59 +0700

    New update
    0.0.3 Beta Version File

---
+++ update.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+clear
+# Đặt biến phiên bản hiện tại
+echo "Checking for the latest version...."
+echo ""
+ver="0.0.3_beta"
+
+# Kiểm tra cập nhật từ raw file
+remote_raw="https://raw.githubusercontent.com/BinZFAKE9999/Image-to-GD-for-Termux/main/server-updates.bzot9"
+response=$(curl -s $remote_raw)
+
+# Lấy thông tin từ raw response
+latest_ver=$(echo "$response" | sed -n '1p')
+latest_file=$(echo "$response" | sed -n '2p')
+
+# So sánh phiên bản
+if [ "$latest_ver" == "$ver" ]; then
+  echo "There is no latest version yet! Please check later!"
+  sleep 5
+  exit 0
+fi
+if [ "$latest_ver" == "" ]; then
+  echo "No data from server, please try again later!"
+  sleep 5
+  exit 0
+fi
+# Hiển thị thông báo cập nhật
+echo "The latest version of the tool is available:"
+echo "- Current version: $ver"
+echo "- Latest version: $latest_ver"
+
+# Hỏi người dùng có muốn cập nhật không
+read -p "Do you want updates? [Y]Yes/[N] No: " choice
+if [ "$choice" != "Y" ] && [ "$choice" != "y" ]; then
+  exit 0
+fi
+
+# Cập nhật nếu người dùng đồng ý
+clear
+echo "Updating to version $latest_ver..."
+cd ..
+wget "$latest_file"
+unzip "$latest_file"
+cd "$(echo "$response" | sed -n '3p')"
+echo "Cập nhật thành công! Vui lòng bấm Cirl + C và nhập command bash Main.sh để khởi động lại."
+exit 0
